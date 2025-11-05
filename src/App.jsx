587d2807aadc30449aed9281e8baee79abffd362
andre <11andrecarvalho@gmail.com>
2025-11-05 16:09:16 -0300

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,21 @@ function Slider({ id, value, onChange, min = 0, max = 255 }) {
   />;
 }
 
-function ColorField({ id, value, onChange, min = 0, max = 255 }) {
+function ColorField({ id, value, onChange, classInput = 'color-field', typeInput = 'number', maxLength,min = 0, max = 255 }) {
   async function handleClick() {
     await navigator.clipboard.writeText(value);
   }
-  
+
   return (
     <div className='color-field-container'>
       <input
-        type="number"
+        type={typeInput}
         id={id}
-        className='color-field'
+        className={classInput}
         min={min}
         max={max}
         value={value}
+        maxLength={maxLength}
         onChange={(e) => onChange(e, e.target.value, e.target.getAttribute('id'))} />
       <button
         className='color-field-btn'
@@ -43,11 +44,15 @@ function ColorField({ id, value, onChange, min = 0, max = 255 }) {
   );
 }
 
+function toHEX(r, g, b) {
+  return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+}
+
 export default function ColorPickerApp() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
-
+  const [hex, setHex] = useState(toHEX(red, green, blue));
   useEffect(() => {
 
     const newColor = `rgb(${red}, ${green}, ${blue})`;
@@ -63,26 +68,55 @@ export default function ColorPickerApp() {
 
     else if (value > 255) { value = 255 }
 
+    value = parseInt(value);
+
+    let nextRed = red;
+    let nextGreen = green;
+    let nextBlue = blue;
+
     switch (color) {
       case 'red':
         setRed(value);
+        nextRed = value;
         break;
       case 'green':
         setGreen(value);
+        nextGreen = value;
         break;
       case 'blue':
         setBlue(value);
+        nextBlue = value;
         break;
       case 'red-field':
         setRed(value);
+        nextRed = value;
         break;
       case 'green-field':
         setGreen(value);
+        nextGreen = value;
         break;
       case 'blue-field':
         setBlue(value);
+        nextBlue = value;
         break;
     }
+
+    setHex(toHEX(nextRed, nextGreen, nextBlue));
+  }
+
+  function handleHexChange(e, value, color) {
+    setHex(value);
+
+    value = value.replace('#', '')
+
+    const r = parseInt(value.slice(0, 2), 16);
+    const g = parseInt(value.slice(2, 4), 16);
+    const b = parseInt(value.slice(4, 6), 16);
+
+    if (!isNaN(r)) setRed(r)
+    if (!isNaN(g)) setGreen(g)
+    if (!isNaN(b)) setBlue(b)
+
   }
 
   return (
@@ -96,7 +130,15 @@ export default function ColorPickerApp() {
         <ColorField id='red-field' onChange={handleChange} value={red} />
         <ColorField id='green-field' onChange={handleChange} value={green} />
         <ColorField id='blue-field' onChange={handleChange} value={blue} />
-        <ColorField id='hex-field' onChange={handleChange} value={blue} />
+
+        <ColorField
+          id='hex-field'
+          onChange={handleHexChange}
+          typeInput='text'
+          classInput='color-field hex-field'
+          value={hex}
+          maxLength={7}
+        />
       </div>
     </>
   );
